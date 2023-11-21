52179349dda7b97fc4d79b55dd01affc2c0d57c0
Gabriel Teodoro <gabriel.rteodoro@yahoo.com.br>
2023-11-20 21:12:33 -0300

    att

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,16 @@
-var slides = document.querySelectorAll('.slide');
-var currentIndex = 0;
-var intervalTime = 3000;
+const box = document.querySelector(".container");
+const imagens = document.querySelectorAll(".container img");
 
-function showSlide(index) {
- slides[currentIndex].classList.remove('active');
- slides[index].classList.add('active');
-  currentIndex = index;
+let contador = 0;
+
+function slider() {
+  contador++;
+
+  if (contador > imagens.length - 1) {
+    contador = 0;
+  }
+
+  box.style.transform = `translateX(${-contador * 1450}px)`;
 }
 
-function nextSlide() {
- showSlide(currentIndex + 1);
- if (currentIndex + 1 == slides.length) {
-    currentIndex = -1;
- }
-}
-
-function prevSlide() {
- showSlide(currentIndex - 1);
- if (currentIndex - 1 == -1) {
-    currentIndex = slides.length;
- }
-}
-
-document.querySelector('.next').addEventListener('click', function() {
- nextSlide();
-});
-
-document.querySelector('.prev').addEventListener('click', function() {
- prevSlide();
-});
-
-// automated slide show
-setInterval(function() {
- nextSlide();
-}, intervalTime);
+setInterval(slider, 2000);
