67826a1e30b6eae669d84ed9f733643543692eec
Gabriel <gabriel.rteodoro@yahoo.com.br>
2024-02-18 20:38:36 -0300

    :zap: perf: Melhoria no tempo de resposta

--- assets/js/jogo.js
+++ assets/js/jogo.js
@@ -1,142 +1,126 @@
-// Definindo minhas variáveis com as keys da API
+const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer');
 
-const arrayImagens1 = []
-const arrayImagens2 = []
-var escolhidas = []
+const characters = [
+  'iron-man',
+  'thor',
+  'dr-estranho',
+  'spider-man',
+  'hulk',
+  'capitao-america',
+  'viuva-negra',
+  'visao',
+  'feiticeira',
+  'pantera-negra',
+];
 
-var clicou = false
-var clicou2 = false
-var pontos = 0
+const createElement = (tag, className) => {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
 
-// Utilizando o fetch para fazer a chmada da API + o then para ter o retorno do meu objeto JSON
-fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=1706283788&apikey=a21b640101cab297641185b0e0c419a4&hash=e56bad61e029119eecdc7a141e48ffec&limit=20)`
-    ).then((response) => {
-        return response.json();        
-    }).then((jsonParsed) => {
+let firstCard = '';
+let secondCard = '';
 
-        for (let i = 0; i < 10; i++){
-            adicionaElemento(arrayImagens1, jsonParsed.data.results[i].thumbnail.path + '.' + jsonParsed.data.results[i].thumbnail.extension)
-            adicionaElemento(arrayImagens2, jsonParsed.data.results[i].thumbnail.path + '.' + jsonParsed.data.results[i].thumbnail.extension)
-        }
+const checkEndGame = () => {
+  const disabledCards = document.querySelectorAll('.disabled-card');
 
-        embaralhaElementos(arrayImagens1)
-        embaralhaElementos(arrayImagens2)
-    }).then(() => {
-        for (let i = 0; i < 10; i++){
-            geraItens(i)
-        }
-    })
+  if (disabledCards.length === 20) {
+    clearInterval(this.loop);
+    alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi de: ${timer.innerHTML}`);
+  }
+}
 
-// Função pra gerar os itens na tela 
-function geraItens(i){
-    let carta = document.createElement('img')
-    let carta2 = document.createElement('img')
-    let containerCartas = document.getElementById('divContainer')
+const checkCards = () => {
+  const firstCharacter = firstCard.getAttribute('data-character');
+  const secondCharacter = secondCard.getAttribute('data-character');
 
-    viraCostas(carta)
-    viraCostas(carta2)
-    
-    // Função onde irá ficar a funcionalidade do game
-    function jogo(){
-        carta.addEventListener('click' , eventoCartao1)
-        carta2.addEventListener('click' , eventoCartao2)
+  if (firstCharacter === secondCharacter) {
 
-        // Verificando se os valores das cartas clicadas são iguais
-        function verifica(){
-            if ( escolhidas.length == 2 ){
-                let cartaa1 = escolhidas[0]
-                let cartaa2 = escolhidas[1]
-    
-                setTimeout(() => {
-                    if (cartaa1.src == cartaa2.src){ 
-                        // Deixando a carta branca quando clicar em SRC iguais e removendo o evento de click                       
-                        cartaa1.src = '/assets/img/branco.png'
-                        cartaa2.src = '/assets/img/branco.png'
-                        cartaa1.style.border = 'none'
-                        cartaa2.style.border = 'none'
-                        cartaa1.removeEventListener('click', eventoCartao1)
-                        cartaa2.removeEventListener('click', eventoCartao2)
-                        cartaa1.style.cursor = 'default'
-                        cartaa2.style.cursor = 'default'
-                        
-                        // Adicionando pontuação
-                        addPontos()
-                    }else{
-                        viraCostas(cartaa1)
-                        viraCostas(cartaa2)
-                    }
-                    escolhidas = []
-                }, 400);
-            } 
-        }
-        
-        function eventoCartao1(){
-            if (carta.src != 'assets/img/branco.png'){
-                carta.id = i
-                carta.src = arrayImagens1[i]
-                escolhidas.push(carta)
-            }
-            verifica()
-        }
+    firstCard.firstChild.classList.add('disabled-card');
+    secondCard.firstChild.classList.add('disabled-card');
 
-        function eventoCartao2(){
-            if (carta2.src != '/assets/img/branco.png'){
-                carta2.id = i
-                carta2.src = arrayImagens2[i]
-                escolhidas.push(carta2)
-            }
-            verifica()
-        }
-    }
+    firstCard = '';
+    secondCard = '';
 
-    jogo()
-    containerCartas.appendChild(carta)
-    containerCartas.appendChild(carta2)
+    checkEndGame();
+
+  } else {
+    setTimeout(() => {
+
+      firstCard.classList.remove('reveal-card');
+      secondCard.classList.remove('reveal-card');
+
+      firstCard = '';
+      secondCard = '';
+
+    }, 500);
+  }
 
 }
 
-// Função para mostrar o verso da "carta", no caso mudar o src da img
-function viraCostas(carta){
-    carta.src = '/assets/img/costasCarta.png'
+const revealCard = ({ target }) => {
+
+  if (target.parentNode.className.includes('reveal-card')) {
+    return;
+  }
+
+  if (firstCard === '') {
+
+    target.parentNode.classList.add('reveal-card');
+    firstCard = target.parentNode;
+
+  } else if (secondCard === '') {
+
+    target.parentNode.classList.add('reveal-card');
+    secondCard = target.parentNode;
+
+    checkCards();
+
+  }
 }
 
-// Add elemento no array
-function adicionaElemento(array, elemento){
-    array.push(elemento)
+const createCard = (character) => {
+
+  const card = createElement('div', 'card');
+  const front = createElement('div', 'face front');
+  const back = createElement('div', 'face back');
+
+  front.style.backgroundImage = `url('assets/imgjogo/${character}.png')`;
+
+  card.appendChild(front);
+  card.appendChild(back);
+
+  card.addEventListener('click', revealCard);
+  card.setAttribute('data-character', character)
+
+  return card;
 }
 
-// Função para embaralhar o aray
-function embaralhaElementos(array) {
-    var m = array.length, t, i;
-  
-    while (m) {
-        i = Math.floor(Math.random() * m--);
-        t = array[m];
-        array[m] = array[i];
-        array[i] = t;
-    }
-  
-    return array;
+const loadGame = () => {
+  const duplicateCharacters = [...characters, ...characters];
+
+  const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
+
+  shuffledArray.forEach((character) => {
+    const card = createCard(character);
+    grid.appendChild(card);
+  });
 }
 
-// Função para adicionar os pontos
-function addPontos(){
-    let textoPontos = document.getElementById('totalPontos')
-    pontos += 1
-    textoPontos.innerHTML = pontos
-    if (pontos == 10){
-        mostraFinal()
-    }
+const startTimer = () => {
+
+  this.loop = setInterval(() => {
+    const currentTime = +timer.innerHTML;
+    timer.innerHTML = currentTime + 1;
+  }, 1000);
+
 }
 
-// Função para adicionar o final ao chegar nos 10 pontos
-function mostraFinal(){
-    let containerCartas = document.getElementById('divContainer')
-    let containerFinal = document.getElementById('final')
-    let textoRelogar = document.getElementById('relogar')
-    containerCartas.style.display = 'none'
-    containerFinal.style.display = 'block'
-    textoRelogar.addEventListener('click', () => {
-        document.location.reload()
-    })    
+window.onload = () => {
+  spanPlayer.innerHTML = localStorage.getItem('player');
+  startTimer();
+  loadGame();
 }
--- assets/js/search.js
+++ assets/js/search.js
@@ -1,11 +1,14 @@
 let boxBuscar = document.querySelector('.buscar-box');
 let lupa = document.querySelector('.lupa-buscar');
 let btnFechar = document.querySelector('.btn-fechar');
+const fechar = document.getElementById('fechar');
+
+// Mostrar a caixa de busca quando o usuário clicar no botão da lupa e esconder quando ele clicar fora dela
 
 lupa.addEventListener('click', ()=> {
-    boxBuscar.classList.toggle('ativar')
+    boxBuscar.classList.add('ativar')
 })
 
-btnFechar.addEventListener('click', ()=> {
-    boxBuscar.classList.toggle('ativar')
+fechar.addEventListener('click', ()=> {
+    boxBuscar.classList.remove('ativar')
 })
